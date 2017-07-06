3ea2371bef6c6bb939240dfc3f4a1d794c1d5459
Kosuke Suzuki <brave.ksk@gmail.com>
2017-07-07 06:44:47 +0900

    モデルテストの作成

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+  	@user = User.new(name:"Test User", email:"test@example.com")
+  end
+
+  test "userインスタンスの有効性" do
+  	assert @user.valid?
+  end
+
+  test "nameの存在性" do
+  	@user.name = ""
+  	assert_not @user.valid?
+  end
+
+  test "emailの存在性" do
+  	@user.email = "     "
+  	assert_not @user.valid?
+  end
 end
