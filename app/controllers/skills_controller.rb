0e4464d25190b95295ef766965209cbee4db81b3
Kosuke Suzuki <brave.ksk@gmail.com>
2017-07-08 20:41:28 +0900

    静的ページの作成

--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -1,2 +1,19 @@
 class SkillsController < ApplicationController
+	def create
+		@skill = current_user.skills.build(skill_params)
+		if @skill.save
+			flash[:success] = "スキルが追加されました"
+			redirect_to root_url
+		else
+			render 'users/new'
+		end
+	end
+
+	def destroy
+		
+	end
+
+	def skill_params
+		params.require(:skill).permit(:name)
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+
   get 'users/new'
   resources :users
+  resources :skills, only: [:create, :destroy]
 end
