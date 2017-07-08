862339f98dc1f7dd29a62b45d0a63167d2ef64d9
Kosuke Suzuki <brave.ksk@gmail.com>
2017-07-08 21:49:50 +0900

    ログイン機能の実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,16 @@ class UsersController < ApplicationController
   	@user = User.find(params[:id])
   	@skills = @user.skills
   end
+
+  def create
+  	@user = User.new(user_params)
+  	if @user.save
+  		flash[:success] = "Welcome!"
+  		redirect_to @user
+  	else
+  		render 'new'
+  	end
+  end
   private
 	  def user_params
 	  	params.require(:user).permit(:name, :email, :password, :password_confirmation)
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'static_pages#home'
 
   get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
   resources :users
   resources :skills, only: [:create, :destroy]
 end
