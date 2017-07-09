f164049fcbac1f0a26fba6c0191992111caea0cc
Kosuke Suzuki <brave.ksk@gmail.com>
2017-07-09 13:42:04 +0900

    skillの削除機能の実装

--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -1,20 +1,29 @@
 class SkillsController < ApplicationController
 	before_action :logged_in_user, only: [:create, :destroy]
+	before_action :correct_user, only: :destroy
 	def create
-		@skill = current_user.skills.build(skill_params)
+		@skill = correct_user.skills.build(skill_params)
 		if @skill.save
 			flash[:success] = "スキルが追加されました"
 			redirect_to root_url
 		else
+			@feed_items = []
 			render 'users/new'
 		end
 	end
 
 	def destroy
-		
+		@skill.destroy
+		flash[:success] = "スキル削除"
+		redirect_to request.referrer || root_url
 	end
 
 	def skill_params
 		params.require(:skill).permit(:name)
+	end
+
+	def correct_user
+		@skill = current_user.skills.find_by(id: params[:id])
+		redirect_to root_url if @skill.nil?
 	end
 end
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,8 @@
 class StaticPagesController < ApplicationController
   def home
-  	@skill = current_user.skills.build if logged_in?
-  	@feed_items = current_user.feed.paginate(page: params[:page])
+  	if logged_in?
+  		@skill = current_user.skills.build
+  		@feed_items = current_user.feed.paginate(page: params[:page])
+  	end
   end
 end
