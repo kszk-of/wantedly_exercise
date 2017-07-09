ebacb2d1a36bca65053332af050cafb7254d50cf
Kosuke Suzuki <brave.ksk@gmail.com>
2017-07-09 19:44:24 +0900

    like機能の実装

---
+++ app/controllers/likes_controller.rb
@@ -0,0 +1,12 @@
+class LikesController < ApplicationController
+  def create
+    @like = Like.create(user_id: current_user.id, skill_id: params[:skill_id])
+    @likes = Like.where(skill_id: params[:skill_id])
+  end
+
+  def destroy
+    like = Like.find_by(user_id: current_user.id, skill_id: params[:skill_id])
+    like.destroy
+    @likes = Like.where(skill_id: params[:skill_id])
+  end
+end
